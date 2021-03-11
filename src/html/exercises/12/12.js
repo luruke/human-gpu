ca7d8b9f2144e6e1a655a8317dc1471caca96a5b
Luigi De Rosa <lurukee@gmail.com>
2021-03-11 13:24:50 +0100

    wip

--- src/html/exercises/12/12.js
+++ src/html/exercises/12/12.js
@@ -1,29 +1,23 @@
 const BUFFERS = `{
   "data": [
-    -0.5, -0.5, -0.5, 0.5, 0.5, -0.5,
-    -0.5, 0.5, 0.5, 0.5, 0.5, -0.5
+    -0.5, -0.5, 0.5, 0.6, 0.6, 2, -0.5, 0.5, 1.0
   ]
 }`;
 
 const ATTRIBUTES = `{
-  "position": { "buffer": "data", "size": 2 }
+  "aData": { "buffer": "data", "size": 3 }
 }`;
 
 const UNIFORMS = `{
-  "offset": [0.1, 0.5],
-  "stretch": 0.5
+  "uDepth": 0.4
 }`;
 
-const VERTEX = `attribute vec2 position;
-uniform vec2 offset;
-uniform float stretch;
+const VERTEX = `attribute vec3 aData;
+uniform float uDepth;
 
 void main() {
-  vec2 pos = position;
-  pos += offset;
-  pos.y += pos.x * stretch;
-
-  gl_Position = vec4(pos, 0.0, 1.0);
+  vec2 pos = aData.xy;
+  gl_Position = vec4(pos, uDepth, aData.z);
 }`;
 
 const FRAGMENT = `precision highp float;
@@ -34,7 +28,8 @@ void main() {
 
 const HIDE_FRAGMENT = true;
 const TITLE = "Human GPU #0012 – what about 3D?";
-const TIPS = `0101010101011010101011010100001!
+const TIPS = `0101010101011010101011010100001! 0101001011?
+
 Ah, it's you human sorry...sometimes i forgot that you don't talk our languge.
 
 Until now we only draw stupid 2D shapes, and maybe you are wondering, how do we render those 3D cool stuff!??
@@ -48,7 +43,7 @@ But let's go step by step!
 
 By now we only modified the \`gl_Position\` \`x\` and \`y\` property.
 
-You might think that the \`z\` property will change the perspective and depth of the vertex, but this actully will only be used to create the "\`depth buffer\`" and has no impact on the position of the vertex.
+You might think that the \`z\` property will change the perspective and depth of the vertex, but this actully will only be used to create the "\`depth buffer\`" and has no impact at all on the position of the vertex.
 Think about it like the CSS \`z-index\` value.
 It's used to determinate visibility when verticies are overlapping.
 Notice that when \`z\` goes outside the -1/+1 range, the vertex is discarded.
@@ -56,12 +51,13 @@ Notice that when \`z\` goes outside the -1/+1 range, the vertex is discarded.
 The \`w\` property (the last value in the \`vec4\`) is also called \`homogeneous coordinate\`. Before processing the \`x, y, z\` we must divide them by the \`w\` component.
 By now it always had \`1.0\` value, so, without knowing, we always divided \`x, y and z\` by \`1.0\` – which had no effect.
 It's pretty stupid right? Yes, and I agree with you. But there are reasons behind it
-`;
+
+Let me see..draw me a triangle!`;
 
 //It's important to note those are some industry common approach and "best-practice", but WebGL/glsl are completely agnostic about this.
 
 const RUN = (gl) => {
-  gl.drawArrays(gl.TRIANGLES, 0, 2 * 3);
+  gl.drawArrays(gl.TRIANGLES, 0, 1 * 3);
 };
 
 var style = document.createElement("style");
@@ -71,8 +67,8 @@ style.innerHTML = `
 }
 
 .canvas {
-  width: 100mm;
-  height: 100mm;
+  width: 90mm;
+  height: 90mm;
 }
 `;
 document.head.appendChild(style);
